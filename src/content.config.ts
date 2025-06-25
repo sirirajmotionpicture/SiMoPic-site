07a80d1955677982ae277b6a6b1a3ca354edaeeb
WarisLapp <waris.lapp@gmail.com>
2025-06-26 00:29:09 +0700

    fix all collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,18 +2,42 @@
 import { defineCollection, z } from 'astro:content';
 
 // 2. Import loader(s)
-import { glob, file } from 'astro/loaders';
+import { glob } from 'astro/loaders';
 
 // 3. Define your collection(s)
 const filmsCollection = defineCollection({
     loader: glob({ pattern:"*.md", base: "./src/content/films" }),
-    schema: ({ image }) => z.object({
-        cover: image(),
-  }),
-});
+    schema: 
+        ({ image }) => z.object({
+            title: z.string(),
+            date: z.string(),
+            time: z.string(),
+            projectname: z.string(),
+            projectlink: z.string(),
+            genre: z.array(z.string()),
+            runtimehour: z.number(),
+            runtimemin: z.number(),
+            runtimesec: z.number(),
+            watchlink: z.string(),
+            cover: image(),
+        })
+})
 
 const projectsCollection = defineCollection({
-    loader: glob({ pattern:"*.md", base: "./src/content/projects" })
+    loader: glob({ pattern:"*.md", base: "./src/content/projects" }),
+    schema: 
+        ({ image }) => z.object({
+            title: z.string(),
+            datefrom: z.string(),
+            timefrom: z.string(),
+            dateto: z.string(),
+            timeto: z.string(),
+            place: z.string(),
+            regisfrom: z.string(),
+            registo: z.string(),
+            regislink: z.string(),
+            cover: image(),
+        }) 
 })
 
 // 4. Export a single `collections` object to register your collection(s)
